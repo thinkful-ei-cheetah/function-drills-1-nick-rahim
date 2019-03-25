51f68c0f8e25aea9881a992e436969fcb8e2ec85
Nick Lee <leexnick@gmail.com>
2019-03-25 12:01:12 -0700

    add type check

--- practice.js
+++ practice.js
@@ -9,13 +9,25 @@ function getYearOfBirth(age){
 }
 
 function createGreeting(name, age) {
+  if(!name || age === undefined) {
+    throw new Error('Arguments not valid');
+  }
+
+  if(typeof name !== 'string') {
+    throw new TypeError('Name must be a String');
+  }
+
+  if(typeof age !== 'number') {
+    throw new TypeError('Age must be a number');
+  }
+
   let yearOfBirth = getYearOfBirth(age);
   return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${yearOfBirth}`;
 }
 
 try {
-  const greeting1 = createGreeting('Nick', 90);
+  const greeting1 = createGreeting('Nick', 0);
   console.log(greeting1);
 } catch (e) {
-  console.error(e);
+  console.error(e.message);
 }
