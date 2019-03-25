60fef1af953451ce0f90c80e5244d84cea523a06
Nick Lee <leexnick@gmail.com>
2019-03-25 11:40:01 -0700

    add yearOfBirth

--- practice.js
+++ practice.js
@@ -1,8 +1,9 @@
 'use strict';
 
 function createGreeting(name, age) {
-  return `Hi, my name is ${name} and I'm ${age} years old`;
+  let yearOfBirth = 2019 - age;
+  return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${yearOfBirth}`;
 }
 
-const greeting1 = createGreeting();
+const greeting1 = createGreeting('Nick', 90);
 console.log(greeting1);
